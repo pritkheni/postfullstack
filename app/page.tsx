250fe5eca9a1b52a57cbacafa143b2b1d0cc7099
pritkheni <pritkheni@gmail.com>
2024-02-03 16:53:39 +0530

    add axios base url

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,37 @@
-'use client'
+"use client";
 
 import axios, { all } from "axios";
 import CreatePost from "./components/CreatePost";
 import { useQuery } from "@tanstack/react-query";
 import Post from "./components/Post";
 import { PostType } from "./types/PostType";
-const allPost = async ()=>{
-  const response = await axios.get("http://localhost:3000/api/posts/getPost")
-  return response.data
-}
+const allPost = async () => {
+  const response = await axios.get("/api/posts/getPost");
+  return response.data;
+};
 export default function Home() {
-  const {data,isLoading,error} = useQuery<PostType[]>({queryKey:["posts"],queryFn:allPost})
-  if(error)return error
-  if(isLoading)return "Loading..."
+  const { data, isLoading, error } = useQuery<PostType[]>({
+    queryKey: ["posts"],
+    queryFn: allPost,
+  });
+  if (error) return error;
+  if (isLoading) return "Loading...";
   console.log(data);
-  
+
   return (
     <main>
-      <CreatePost/>
-      
-      {data?.map((post)=> <Post key={post.id} comments={post.Comment} name={post.user.name} avatar={post.user.image} postTitle={post.title} id={post.id}/>)}
+      <CreatePost />
+
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          comments={post.Comment}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          id={post.id}
+        />
+      ))}
     </main>
   );
 }
